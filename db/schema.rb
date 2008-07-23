8d1f8be19e1b727e08fd822636393969f3d85057
spearwolf <wolfger@spearwolf.de>
2008-07-23 17:43:36 +0200

    removed rails 2.1 obsolete configs

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of ActiveRecord to incrementally modify your database, and
+# please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your database schema. If you need
@@ -12,13 +12,13 @@
 ActiveRecord::Schema.define(:version => 3) do
 
   create_table "scheduled_messages", :force => true do |t|
-    t.string   "destination_queue", :default => "", :null => false
-    t.string   "schedule_method",   :default => "", :null => false
-    t.string   "schedule_value",    :default => "", :null => false
-    t.text     "message",           :default => "", :null => false
+    t.string   "destination_queue",               :null => false
+    t.string   "schedule_method",                 :null => false
+    t.string   "schedule_value",                  :null => false
+    t.text     "message",                         :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "job_id"
+    t.integer  "job_id",            :limit => 11
   end
 
 end
